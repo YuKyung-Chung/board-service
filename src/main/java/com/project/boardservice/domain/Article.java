88522e17518c7d485ba5a56611ab27b4ccb33e6d
YuKyung-Chung <kadie@naver.com>
2023-05-02 00:03:57 +0900

    #5 - import문 수정

--- src/main/java/com/project/boardservice/domain/Article.java
+++ src/main/java/com/project/boardservice/domain/Article.java
@@ -1,17 +1,10 @@
 package com.project.boardservice.domain;
 
-import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
